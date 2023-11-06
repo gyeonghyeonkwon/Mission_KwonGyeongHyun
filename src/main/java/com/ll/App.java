aba6ba1c03a22e943ec6188f4627454d212ff0d3
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-11-06 10:24:09 +0900

    1단계완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,6 +1,21 @@
 package com.ll;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+import java.util.SortedMap;
+
 public class App {
     public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("==명언앱==");
+        while (true) {
+            System.out.print("명령 :");
+            String sc = scanner.nextLine();
+
+            if (sc.equals("종료")){
+                break;
+            }
+        }
     }
 }
