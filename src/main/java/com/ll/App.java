b5320f811b436cc180d2173cc418567482af6c62
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-11-06 11:49:10 +0900

    6단계 삭제?id=2 까지구현

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -14,6 +14,7 @@ public class App {
             String sc = scanner.nextLine();
 
             if (sc.equals("종료")){
+                System.out.println("프로그램을 종료합니다.");
                 break; //반복문 종료
             }
             else if (sc.equals("등록")) {
@@ -21,11 +22,11 @@ public class App {
                 String wiseSaying = scanner.nextLine();
 
                 System.out.print("작가 :");
-                String another = scanner.nextLine();
+                String author = scanner.nextLine();
                 wiseSayingNumber++; // 정수 증가
                 int id = wiseSayingNumber; // 증가한 값 id 변수에 저장
                 System.out.printf("%d번 명언이 등록되었습니다.%n" ,id);
-                Inventory inventory = new Inventory(id ,another , wiseSaying); // 객체 생성 선언
+                Inventory inventory = new Inventory(id ,author , wiseSaying); // 객체 생성 선언 , 생성자 매개 변수전달
                 inventoryList.add(inventory); // 리스트에 클래스 객체들을 저장 ( id ,another , wiseSaying)
             }
             else if (sc.equals("목록")) {
@@ -33,9 +34,26 @@ public class App {
                 System.out.println("-".repeat(18));
                 for (int i = inventoryList.size() -1 ; i >= 0; i--){ // 리스트 사이즈 -1 만큼 역순순회
                     Inventory inventory = inventoryList.get(i); // 리스트에 저장되어있는 값을 꺼내어 클래스의 객체에저장
-                    System.out.printf("%d %s %s %n" , inventory.id , inventory.another , inventory.wiseSaying);
+                    System.out.printf("%d %s %s %n" , inventory.id , inventory.author , inventory.wiseSaying);
                 }
+            }
+            else if (sc.startsWith("삭제?")){
+                String [] scBit = sc.split("\\?",2);
+                String scAction = scBit[0];
+                String queryString = scBit[1];
+                String[] queryStringBit = queryString.split("&"); //["id=2"]
+                int id =0;
+                for (int i =0; i < queryStringBit.length; i++){  // queyStringBit 길이만큼 순회
+                    String queryStringStr = queryStringBit[i];
+                    String [] queryStringParam = queryStringStr.split("=",2); // "id" , "2"
+                    String ParamName = queryStringParam[0]; // "id"
+                    String ParamValue = queryStringParam[1]; // "2"
 
+                    if (ParamName.equals("id")){
+                        id = Integer.parseInt(ParamValue);
+                    }
+                    System.out.printf("%d 번 명언이 삭제되었습니다%n",id);
+                 }
             }
         }
     }
--- src/main/java/com/ll/Inventory.java
+++ src/main/java/com/ll/Inventory.java
@@ -2,12 +2,12 @@ package com.ll;
 
 public class Inventory {
     int  id; //번호
-    String another; //작가
+    String author; //작가
     String wiseSaying; //명언
-    public Inventory(int id, String another, String wiseSaying) { //생성자 초기화
+    public Inventory(int id, String author, String wiseSaying) { //생성자 초기화
 
         this.id = id;
-        this.another = another;
+        this.author = author;
         this.wiseSaying =wiseSaying;
     }
 }
